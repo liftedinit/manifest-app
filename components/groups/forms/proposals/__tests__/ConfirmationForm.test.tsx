3071081e205c852c4f8e8e13d57086ea2cffafe4
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-11-07 14:22:17 -0500

    fix: proposal confirmation form test

--- components/groups/forms/proposals/__tests__/ConfirmationForm.test.tsx
+++ components/groups/forms/proposals/__tests__/ConfirmationForm.test.tsx
@@ -43,17 +43,14 @@ describe('ConfirmationModal Component', () => {
 
   test('renders component with correct details', () => {
     renderWithChainProvider(<ConfirmationModal {...mockProps} />);
-    expect(screen.getByText('Confirmation')).toBeInTheDocument();
-    expect(screen.getByLabelText('proposal-details')).toBeInTheDocument();
     expect(screen.getByText(mockProposalFormData.title)).toBeInTheDocument();
     expect(screen.getByText('manifest1hj5fv...8ws9ct')).toBeInTheDocument();
 
-    // TODO: This is never displayed in the component
-    // expect(screen.getByText(mockFormData.summary)).toBeInTheDocument();
-
-    expect(screen.getByText('MESSAGES')).toBeInTheDocument();
-    expect(screen.getByText('METADATA')).toBeInTheDocument();
-    expect(screen.getByLabelText('meta-details')).toBeInTheDocument();
+    expect(screen.getByText('Messages')).toBeInTheDocument();
+    expect(screen.getByText('Type')).toBeInTheDocument();
+    expect(screen.getByText('send')).toBeInTheDocument();
+    expect(screen.getByText('Metadata')).toBeInTheDocument();
+    expect(screen.getByText(mockProposalFormData.metadata.authors)).toBeInTheDocument();
     expect(screen.getByText(mockProposalFormData.metadata.title)).toBeInTheDocument();
     expect(screen.getByText(mockProposalFormData.metadata.summary)).toBeInTheDocument();
     expect(screen.getByText(mockProposalFormData.metadata.details)).toBeInTheDocument();
@@ -61,7 +58,7 @@ describe('ConfirmationModal Component', () => {
 
   test('calls prevStep when "Prev: Metadata" button is clicked', () => {
     renderWithChainProvider(<ConfirmationModal {...mockProps} />);
-    const prevButton = screen.getByText('Prev: Metadata');
+    const prevButton = screen.getByText('Back: Metadata');
     fireEvent.click(prevButton);
     expect(mockProps.prevStep).toHaveBeenCalled();
   });
