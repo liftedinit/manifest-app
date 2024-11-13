ec9deb87f2be6677478eb4b185f31945174a49ba
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-11-13 10:31:58 -0500

    fix: denom info modal rendering

--- components/factory/modals/denomInfo.tsx
+++ components/factory/modals/denomInfo.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { TruncatedAddressWithCopy } from '@/components/react/addressCopy';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 import { MetadataSDKType } from '@liftedinit/manifestjs/dist/codegen/cosmos/bank/v1beta1/bank';
-import { useRouter } from 'next/router';
 
 export const DenomInfoModal: React.FC<{
   denom: MetadataSDKType | null;
@@ -28,50 +27,13 @@ export const DenomInfoModal: React.FC<{
         </form>
         <h3 className="text-xl font-semibold text-[#161616] dark:text-white mb-6">Denom Details</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div>
-            <InfoItem label="NAME" value={denom?.name ?? 'No name available'} />
-            <InfoItem label="SYMBOL" value={denom?.symbol ?? 'No symbol available'} />
-            {denom?.description && (
-              <InfoItem
-                label="DESCRIPTION"
-                value={denom?.description ?? 'No description available'}
-              />
-            )}
-            {denom?.denom_units[1]?.exponent && (
-              <InfoItem
-                label="EXPONENT"
-                value={denom?.denom_units[1]?.exponent?.toString() ?? '0'}
-              />
-            )}
-          </div>
-          <div>
-            {denom?.denom_units?.map(
-              (
-                unit: {
-                  denom: string;
-                  aliases?: string[];
-                  exponent?: number;
-                },
-                index: number
-              ) => (
-                <div key={index} className="mb-4">
-                  <InfoItem label="DENOM" value={unit?.denom} />
-                  <InfoItem label="ALIASES" value={unit?.aliases?.join(', ') || 'No aliases'} />
-                </div>
-              )
-            )}
-          </div>
-        </div>
-        <h4 className="text-lg font-semibold text-[#161616] dark:text-white mt-6  mb-4">
-          Additional Information
-        </h4>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <InfoItem label="Name" value={denom?.name ?? 'No name available'} />
+          <InfoItem label="Symbol" value={denom?.symbol ?? 'No symbol available'} />
           <InfoItem
-            label="BASE"
-            value={denom?.base ? decodeURIComponent(denom.base) : ''}
-            isAddress={true}
+            label="Description"
+            value={denom?.description ?? 'No description available'}
+            className="col-span-2 row-span-2"
           />
-          <InfoItem label="DISPLAY" value={denom?.display ?? 'No display available'} />
         </div>
       </div>
       <form method="dialog" className="modal-backdrop" onSubmit={onClose}>
@@ -85,15 +47,17 @@ function InfoItem({
   label,
   value,
   isAddress = false,
+  className = '',
 }: {
   label: string;
   value: string;
   isAddress?: boolean;
+  className?: string;
 }) {
   return (
-    <div className="mb-4 flex flex-col">
+    <div className={`mb-4 flex flex-col ${className}`}>
       <p className="text-sm font-semibold text-[#00000099] dark:text-[#FFFFFF99] mb-2">{label}</p>
-      <div className="bg-base-300 rounded-[16px] p-4 flex-grow">
+      <div className="bg-base-300 rounded-[16px] p-4 flex-grow h-full">
         {isAddress ? (
           <div className="flex items-center">
             <TruncatedAddressWithCopy address={value} slice={8} />
@@ -104,7 +68,7 @@ function InfoItem({
               rel="noopener noreferrer"
               className="ml-2 text-primary hover:text-primary/50"
             >
-              <FaExternalLinkAlt aria-hidden="true" />{' '}
+              <FaExternalLinkAlt aria-hidden="true" />
               <span className="sr-only">External link</span>
             </a>
           </div>
