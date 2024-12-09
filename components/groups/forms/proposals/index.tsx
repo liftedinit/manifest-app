215915e4459e52f604684fabc03305992474d0ea
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2024-12-09 16:02:20 -0500

    chore: cleanup (#144)

--- components/groups/forms/proposals/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from './ConfirmationForm';
-export * from './ProposalMessages';
-export * from './ProposalMetadataForm';
-export * from './ProposalDetailsForm';
-export * from './messages';
