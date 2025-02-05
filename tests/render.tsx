e81a6d9136c91c76a3bb84302ae66682274055f7
Joseph Chalabi <chalabi.joseph@gmail.com>
2025-02-05 13:57:25 -0700

    add chain-registry chains to test render

--- tests/render.tsx
+++ tests/render.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { ChainProvider } from '@cosmos-kit/react';
 import { ToastProvider } from '@/contexts';
-import { defaultAssetLists, defaultChain, osmosisChain, osmosisAssetList } from '@/tests/mock';
+
+import {
+  assets as manifestAssets,
+  chain as manifestChain,
+} from 'chain-registry/testnet/manifesttestnet';
+import {
+  assets as osmosisAssets,
+  chain as osmosisChain,
+} from 'chain-registry/testnet/osmosistestnet';
+import { assets as axelarAssets, chain as axelarChain } from 'chain-registry/testnet/axelartestnet';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SkipProvider } from '@/contexts/skipGoContext';
 
 const defaultOptions = {
-  chains: [defaultChain, osmosisChain],
-  assetLists: [...defaultAssetLists, ...osmosisAssetList],
+  chains: [manifestChain, osmosisChain, axelarChain],
+  assetLists: [manifestAssets, osmosisAssets, axelarAssets],
   wallets: [],
 };
 
