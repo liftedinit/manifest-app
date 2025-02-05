b3137a03ce5b31c20781844b4fb5b2b1cad1735b
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-02-05 08:44:20 -0500

    fix: tx history improvements (#229)

--- components/bank/handlers/group/msgSubmitProposalHandler.tsx
+++ components/bank/handlers/group/msgSubmitProposalHandler.tsx
@@ -5,18 +5,36 @@ import { MsgSubmitProposal } from '@liftedinit/manifestjs/dist/codegen/cosmos/gr
 import { TruncatedAddressWithCopy } from '@/components/react/addressCopy';
 import { format } from 'react-string-format';
 
-const createMessage = (template: string, ids: string, sender: string) => {
-  const message = format(template, ids, <TruncatedAddressWithCopy address={sender} slice={24} />);
+const createMessage = (template: string, ids: string, policyAddress: string, sender?: string) => {
+  const message = format(
+    template,
+    ids,
+    policyAddress ? (
+      <TruncatedAddressWithCopy address={policyAddress} slice={24} />
+    ) : (
+      'an unknown address'
+    ),
+    sender ? <TruncatedAddressWithCopy address={sender} slice={24} /> : 'an unknown address'
+  );
   return <span className="flex gap-1">{message}</span>;
 };
 
 export const MsgSubmitProposalHandler = createSenderReceiverHandler({
   iconSender: GroupsIcon,
   successSender: tx =>
-    createMessage('You submitted proposal #{0} to {1}', tx.proposal_ids?.[0], tx.sender), // TODO Link to proposal
+    createMessage(
+      'You submitted proposal #{0} to {1}',
+      tx.proposal_ids?.[0],
+      tx.metadata?.groupPolicyAddress
+    ), // TODO Link to proposal
   failSender: 'You failed to submit a proposal',
   successReceiver: tx =>
-    createMessage('Proposal #{0} was submitted by {1}', tx.proposal_ids?.[0], tx.sender), // TODO Link to proposal
+    createMessage(
+      'Proposal #{0} was submitted to {1} by {2}',
+      tx.proposal_ids?.[0],
+      tx.metadata?.groupPolicyAddress,
+      tx.sender
+    ), // TODO Link to proposal
 });
 
 registerHandler(MsgSubmitProposal.typeUrl, MsgSubmitProposalHandler);
