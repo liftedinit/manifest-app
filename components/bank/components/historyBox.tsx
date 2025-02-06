6b6cef6303d530a35e81a18841f7c4fe8a1e2385
Hans Larsen <hans@larsen.online>
2025-02-06 08:12:58 -0800

    feat: refetch transactions and metadatas at an interval after the last refetch (#245)

--- components/bank/components/historyBox.tsx
+++ components/bank/components/historyBox.tsx
@@ -1,10 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TransactionAmount, TxMessage } from '../types';
 import { shiftDigits, formatLargeNumber, formatDenom } from '@/utils';
 import { getHandler } from '@/components/bank/handlers/handlerRegistry';
 import { MetadataSDKType } from '@liftedinit/manifestjs/dist/codegen/cosmos/bank/v1beta1/bank';
 import { useTokenFactoryDenomsMetadata } from '@/hooks';
 import TxInfoModal from '../modals/txInfo';
+import { useIntervalDebounceEffect } from '@/hooks/useDebounceEffect';
+
+// Interval to refresh the history box transaction and metadata.
+// This is used as a delay between successful queries.
+const HISTORY_BOX_REFRESH_INTERVAL = 2000;
 
 export interface TransactionGroup {
   tx_hash: string;
@@ -36,15 +41,21 @@ export function HistoryBox({
   totalPages: number;
   txLoading: boolean;
   isError: boolean;
-  refetch: () => void;
+  refetch: () => Promise<unknown>;
   skeletonGroupCount: number;
   skeletonTxCount: number;
   isGroup?: boolean;
 }>) {
   const [selectedTx, setSelectedTx] = useState<TxMessage | null>(null);
-  const { metadatas, isMetadatasLoading } = useTokenFactoryDenomsMetadata();
+  const { metadatas, isMetadatasLoading, refetchMetadatas } = useTokenFactoryDenomsMetadata();
 
   const isLoading = initialLoading || txLoading || isMetadatasLoading;
+
+  useIntervalDebounceEffect(
+    () => Promise.all([refetch(), refetchMetadatas()]),
+    HISTORY_BOX_REFRESH_INTERVAL,
+    [refetch, refetchMetadatas]
+  );
 
   function formatDateShort(dateString: string): string {
     const date = new Date(dateString);
