96a782b31343b8b41dabeeb608f834fd7317c248
Hans Larsen <hans@larsen.online>
2025-02-07 10:30:10 -0800

    Remove 1Password from all our input texts
    There might be a place where we want 1Password to allow for showing the
    autofill dialog, but any place I found that was showing it it was more
    annoying.
    
    For example, it shows over the Contact button in the Address input,
    because it wants to fill it with my street address.

--- components/react/inputs/TextInput.tsx
+++ components/react/inputs/TextInput.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { BaseInput } from './BaseInput';
 
 export const TextInput: React.FC<React.ComponentProps<typeof BaseInput>> = props => (
-  <BaseInput type="text" {...props} />
+  <BaseInput type="text" data-1p-ignore {...props} />
 );
