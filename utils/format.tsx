a5a6c195ebe7830b25ae7f239a642257cb252457
Hans Larsen <hans@larsen.online>
2025-02-06 22:28:48 -0800

    Add a new formatDenomWithBadge that returns a ReactNode
    This will add a badge if the denom is verified.
    
    Fixes #250

--- utils/format.tsx
+++ utils/format.tsx
@@ -1,7 +1,9 @@
 import { MetadataSDKType } from '@liftedinit/manifestjs/dist/codegen/cosmos/bank/v1beta1/bank';
 import { shiftDigits } from '@/utils/maths';
 import { denomToAsset } from './ibc';
+import { DenomDisplay, DenomVerifiedBadge } from '@/components/factory/components/DenomDisplay';
 import env from '@/config/env';
+import { ReactNode } from 'react';
 
 export function formatLargeNumber(num: number): string {
   if (!Number.isFinite(num)) return 'Invalid number';
@@ -31,19 +33,33 @@ export function formatLargeNumber(num: number): string {
   return num.toFixed(6);
 }
 
+/**
+ * Format a denom to a display name react node with a verified badge.
+ * @param denom The denom to format.
+ * @param small Whether to use a smaller badge.
+ */
+export function formatDenomWithBadge(denom: string, small?: boolean): ReactNode {
+  const cleanDenom = formatDenom(denom);
+  const classes = `${small ? 'w-3' : 'w-5'} mx-1 inline relative bottom-1 text-primary`;
+
+  return (
+    <>
+      {cleanDenom} <DenomVerifiedBadge base={denom} className={classes} />
+    </>
+  );
+}
+
 export function formatDenom(denom: string): string {
   const assetInfo = denomToAsset(env.chain, denom);
 
   // Fallback to cleaning the denom if no assetInfo
-  const cleanDenom = denom?.replace(/^factory\/[^/]+\//, '');
+  let cleanDenom = denom.replace(/^factory\/[^/]+\//, '');
 
   // Skip cleaning for IBC denoms as they should be resolved via assetInfo
   if (cleanDenom.startsWith('ibc/')) {
-    return assetInfo?.display.toUpperCase() ?? '';
-  }
-
-  if (cleanDenom?.startsWith('u')) {
-    return cleanDenom.slice(1).toUpperCase();
+    cleanDenom = assetInfo?.display.toUpperCase() ?? '';
+  } else if (cleanDenom.startsWith('u')) {
+    cleanDenom = cleanDenom.slice(1).toUpperCase();
   }
 
   return cleanDenom;
