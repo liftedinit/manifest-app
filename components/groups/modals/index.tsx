21cc27400ca80994ce14ca154a89e333b8ad3f85
Hans Larsen <hans@larsen.online>
2025-02-11 23:20:36 -0800

    Remove export of unused component

--- components/groups/modals/index.tsx
+++ components/groups/modals/index.tsx
@@ -1,5 +1,5 @@
 export * from '../../react/authSignerModal';
-export * from './groupDetailsModal';
+//export * from './groupDetailsModal'; // Do we need this?
 export * from './updateGroupModal';
 export * from './voteDetailsModal';
 export * from './voteModal';
