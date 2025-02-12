479f19426cf43af448be66e66565a32c9c7c4340
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-02-12 15:59:20 -0500

    fix: devnet

--- config/manifestChain.ts
+++ config/manifestChain.ts
@@ -1,13 +1,28 @@
 import env from './env';
+import { AssetList, Chain, IBCInfo } from '@chain-registry/types';
 
 const buildType = env.chainTier;
-let manifestModule;
+let manifestModule: any;
 
 // TODO: Add mainnet manifest when it's available
-if (buildType === 'devnet') {
-  manifestModule = await import('chain-registry/devnet/manifestdevnet');
+if (buildType === 'qa') {
+  manifestModule = require('chain-registry/devnet/manifestdevnet');
 } else {
-  manifestModule = await import('chain-registry/testnet/manifesttestnet');
+  manifestModule = require('chain-registry/testnet/manifesttestnet');
 }
 
-export const { assets: manifestAssets, chain: manifestChain, ibc: manifestIbc } = manifestModule;
+let manifestAssets: AssetList, manifestChain: Chain, manifestIbc: IBCInfo[];
+
+if (buildType === 'qa') {
+  const { assets, chain } = manifestModule;
+  manifestAssets = assets;
+  manifestChain = chain;
+  manifestIbc = []; // Default value since devnet doesn't include ibc
+} else {
+  const { assets, chain, ibc } = manifestModule;
+  manifestAssets = assets;
+  manifestChain = chain;
+  manifestIbc = ibc;
+}
+
+export { manifestAssets, manifestChain, manifestIbc };
