7550367a06e375cff09a3c72abb8fe5b5f2853f7
Hans Larsen <hans@larsen.online>
2025-02-16 16:17:11 -0800

    Add tests and fix a bug with useDelayResetState

--- hooks/useDelayResetState.ts
+++ hooks/useDelayResetState.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
 
 /**
  * Create a state that will reset after a delayed to its initial or previous value.
@@ -11,25 +11,32 @@ export function useDelayResetState<T>(
   initialState: T,
   delay: number
 ): [T, Dispatch<SetStateAction<T>>, Dispatch<SetStateAction<T>>] {
-  const [resetState, setResetState] = useState(initialState);
   const [state, setState] = useState(initialState);
+  const resetStateRef = useRef<T>(initialState);
+  const timerRef = useRef<Timer | undefined>();
 
-  const setDelayResetState = useCallback(
-    (value: SetStateAction<T>) => {
-      if (typeof value === 'function') {
-        value = (value as (prevState: T) => T)(state);
-      }
+  const setDelayResetState = (value: SetStateAction<T>) => {
+    if (typeof value === 'function') {
+      value = (value as (prevState: T) => T)(state);
+    }
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
 
-      setState(value);
-      const timer = setTimeout(() => setState(resetState), delay);
-      return () => clearTimeout(timer);
-    },
-    [state, delay, resetState]
-  );
-
-  const updateState = (value: SetStateAction<T>) => {
-    setResetState(value);
+    setState(value);
+    timerRef.current = setTimeout(() => {
+      setState(resetStateRef.current);
+      clearTimeout(timerRef.current);
+      timerRef.current = undefined;
+    }, delay);
   };
 
-  return [state, setDelayResetState, updateState] as const;
+  const updateBaseState = (value: SetStateAction<T>) => {
+    if (typeof value === 'function') {
+      value = (value as (prevState: T) => T)(state);
+    }
+    resetStateRef.current = value;
+  };
+
+  return [state, setDelayResetState, updateBaseState] as const;
 }
