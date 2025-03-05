7f827479e163f20f4fd75fbb0ef00b8ba31bd8f8
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-03-05 09:03:34 -0500

    feat: feegrant msg grand allowance handler (#286)

---
+++ components/bank/handlers/feegrant/msgGrantAllowanceHandler.tsx
@@ -0,0 +1,27 @@
+import { MsgGrantAllowance } from '@liftedinit/manifestjs/dist/codegen/cosmos/feegrant/v1beta1/tx';
+import { format } from 'react-string-format';
+
+import { registerHandler } from '@/components/bank/handlers/handlerRegistry';
+import { MemberIcon } from '@/components/icons/MemberIcon';
+import { TruncatedAddressWithCopy } from '@/components/react/addressCopy';
+
+import { createSenderReceiverHandler } from '../createSenderReceiverHandler';
+
+const createMessage = (template: string, addr: string): JSX.Element => {
+  const message = format(
+    template,
+    addr ? <TruncatedAddressWithCopy address={addr} slice={24} /> : 'an unknown address'
+  );
+  return <span className="flex gap-1">{message}</span>;
+};
+
+export const MsgGrantAllowanceHandler = createSenderReceiverHandler({
+  iconSender: MemberIcon,
+  successSender: tx => createMessage('You granted a fee allowance to {0}', tx.metadata?.grantee),
+  failSender: tx =>
+    createMessage('You failed to grant a fee allowance to {0}', tx.metadata?.grantee),
+  successReceiver: tx =>
+    createMessage('You were granted a fee allowance by {0}', tx.metadata?.granter),
+});
+
+registerHandler(MsgGrantAllowance.typeUrl, MsgGrantAllowanceHandler);
