bebf59312ac016f55b0372538feeab71d679b3e4
Hans Larsen <hans@larsen.online>
2025-03-11 12:22:27 -0700

    Fix the skip-go widget by disabling DSD and proper dialog closing behaviour

--- components/bank/forms/ibcSendForm.tsx
+++ components/bank/forms/ibcSendForm.tsx
@@ -1,6 +1,7 @@
 import { useChain } from '@cosmos-kit/react';
 import { Widget } from '@skip-go/widget';
 import { useRef } from 'react';
+import { ShadowScopeConfigProvider } from 'react-shadow-scope';
 
 import env from '@/config/env';
 import { useTheme } from '@/contexts';
@@ -15,65 +16,69 @@ function IbcSendForm({ token }: { token: string }) {
   // Determine if dark mode is active
   const isDark = theme === 'dark';
 
+  const themeColors = {
+    brandColor: '#a087ff',
+    primary: {
+      background: {
+        normal: isDark ? '#302E41' : '#FCFCFF',
+      },
+      text: {
+        normal: isDark ? '#FFFFFF' : '#161616',
+        lowContrast: isDark ? '#f0f0ff' : '#1d192d',
+        ultraLowContrast: isDark ? '#e0e0ff' : '#f0f0ff',
+      },
+      ghostButtonHover: '#b19aff',
+    },
+    secondary: {
+      background: {
+        normal: '#a087ff',
+        transparent: 'transparent',
+        hover: isDark ? '#a087ff' : '#a087ff',
+      },
+    },
+    success: {
+      text: '#4caf50',
+    },
+    warning: {
+      background: '#ffb300',
+      text: '#161616',
+    },
+    error: {
+      background: '#e53935',
+      text: '#ffffff',
+    },
+  };
+
   return (
     <div ref={containerRef} className="w-[100%] max-w-[500px] px-2 box-border relative">
-      <Widget
-        filter={{
-          source: {
-            [env.chainId]: undefined,
-            [env.osmosisChainId]: undefined,
-            [env.axelarChainId]: undefined,
-          },
-          destination: {
-            [env.chainId]: undefined,
-            [env.osmosisChainId]: undefined,
-            [env.axelarChainId]: undefined,
-          },
-        }}
-        defaultRoute={{
-          srcAssetDenom: token,
-          srcChainId: env.chainId,
-          destChainId: env.osmosisChainId,
-          destAssetDenom: ibcDenom,
-        }}
-        brandColor={'#a087ff'}
-        onlyTestnet={true}
-        connectedAddresses={{
-          [env.chainId]: address,
-        }}
-        theme={{
-          brandColor: '#a087ff',
-          primary: {
-            background: {
-              normal: isDark ? '#302E41' : '#FCFCFF',
+      <ShadowScopeConfigProvider config={{ dsd: 'off' }}>
+        <Widget
+          filter={{
+            source: {
+              [env.chainId]: undefined,
+              [env.osmosisChainId]: undefined,
+              [env.axelarChainId]: undefined,
             },
-            text: {
-              normal: isDark ? '#FFFFFF' : '#161616',
-              lowContrast: isDark ? '#f0f0ff' : '#1d192d',
-              ultraLowContrast: isDark ? '#e0e0ff' : '#f0f0ff',
+            destination: {
+              [env.chainId]: undefined,
+              [env.osmosisChainId]: undefined,
+              [env.axelarChainId]: undefined,
             },
-            ghostButtonHover: '#b19aff',
-          },
-          secondary: {
-            background: {
-              normal: '#a087ff',
-              transparent: 'transparent',
-              hover: isDark ? '#a087ff' : '#a087ff',
-            },
-          },
-          success: {
-            text: '#4caf50',
-          },
-          warning: {
-            background: '#ffb300',
-            text: '#161616',
-          },
-          error: {
-            background: '#e53935',
-            text: '#ffffff',
-          },
-        }}
-      />
+          }}
+          defaultRoute={{
+            srcAssetDenom: token,
+            srcChainId: env.chainId,
+            destChainId: env.osmosisChainId,
+            destAssetDenom: ibcDenom,
+          }}
+          brandColor={'#a087ff'}
+          onlyTestnet={true}
+          connectedAddresses={{
+            [env.chainId]: address,
+          }}
+          theme={themeColors}
+        />
+      </ShadowScopeConfigProvider>
     </div>
   );
 }
--- components/bank/modals/sendModal.tsx
+++ components/bank/modals/sendModal.tsx
@@ -30,7 +30,21 @@ export default function SendModal({
   return (
     <ModalDialog
       open={isOpen}
-      onClose={() => setOpen?.(false)}
+      onClose={() => {
+        // skip-go/widget does not have a lot of useful props/events so we kind of
+        // have to hack our way around it.
+        const allInputs = [...document.querySelectorAll('react-shadow-scope')].map(s =>
+          s.shadowRoot?.querySelector('div[open]')
+        );
+        const isSkipGoDialogOpened = allInputs.filter(Boolean).length > 0;
+
+        // Check that we don't have the widget dialog opened, and prevent closing if we do.
+        if (document.querySelector('wcm-modal') !== null && isSkipGoDialogOpened) {
+          return;
+        }
+
+        setOpen?.(false);
+      }}
       title="Send Assets"
       className="z-10"
     >
