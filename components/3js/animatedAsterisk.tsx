42a0faa4b97b94f1b900a255605278be1687bf35
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-03-26 13:13:24 -0400

    Update components/3js/animatedAsterisk.tsx
    Co-authored-by: Hans Larsen <hans@larsen.online>
    Signed-off-by: Félix C. Morency <1102868+fmorency@users.noreply.github.com>

--- components/3js/animatedAsterisk.tsx
+++ components/3js/animatedAsterisk.tsx
@@ -1,6 +1,6 @@
 import { Environment } from '@react-three/drei';
 import { Canvas, useFrame } from '@react-three/fiber';
-import React, { Suspense, useEffect, useMemo, useRef } from 'react';
+import { Suspense, useEffect, useMemo, useRef } from 'react';
 import { BufferAttribute, BufferGeometry, Mesh, Vector3 } from 'three';
 import * as THREE from 'three';
 
